dc05d2c0b2b0a6287852e29d6803d9a436a7436e
UniverseVG <gmvarun2001@gmail.com>
2024-07-19 15:14:23 +0530

    add day1 challenge

---
+++ day1_variables.js
@@ -0,0 +1,35 @@
+//task 1
+var num = 10;
+console.log(num);
+
+//task 2
+let str = "JavaScript";
+console.log(str);
+
+//task 3
+const bool = true;
+console.log(bool);
+
+//task 4
+let str2 = "Hello";
+let num2 = 10;
+let bool2 = true;
+let obj = {
+  name: "John",
+  age: 30,
+};
+const arr = [1, 2, 3];
+
+console.log(typeof str2, typeof num2, typeof bool2, typeof obj, typeof arr);
+
+// task5
+let temp = 10;
+console.log("before", temp);
+temp = 20;
+console.log("after", temp);
+
+//task6
+const tempConst = 10;
+console.log("before", tempConst);
+tempConst = 20;
+console.log("after", tempConst);
