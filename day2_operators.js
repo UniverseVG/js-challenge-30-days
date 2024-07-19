ced0823d73ba4f4cf3c4cecf48474de3608b08f6
UniverseVG <gmvarun2001@gmail.com>
2024-07-19 16:16:38 +0530

    add day2 challenge

---
+++ day2_operators.js
@@ -0,0 +1,52 @@
+//activity 1
+let num1 = 20;
+let num2 = 10;
+
+let add = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2;
+let mod = num1 % num2;
+
+console.log(add, sub, mul, div, mod);
+
+//activity 2
+let a = 10;
+let add1 = 0;
+let sub1 = 20;
+
+add1 += a;
+sub1 -= a;
+console.log(add1, sub1);
+
+//activity 3
+
+let temp1 = 10;
+let temp2 = 20;
+console.log(temp1 < temp2);
+console.log(temp1 > temp2);
+console.log(temp1 <= temp2);
+console.log(temp1 >= temp2);
+console.log(temp1 == temp2);
+console.log(temp1 != temp2);
+
+//activity 4
+let num3 = 10;
+let num4 = "10";
+console.log(num3 == num4);
+console.log(num3 === num4);
+console.log(num3 !== num4);
+
+//activity 5
+
+let num5 = 10;
+let num6 = 20;
+console.log(num5 < num6 && num5 > num6);
+console.log(num5 < num6 || num5 > num6);
+console.log(!num5);
+
+//activity 6
+let num = 4;
+let str = num > 0 ? "is positive" : "is negative";
+
+console.log(str);
